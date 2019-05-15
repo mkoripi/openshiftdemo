880a7445f566e28c49eddd4261f5eee50d4f4ca9
=Malyadri Koripi <mkoripi@gmail.com>
2019-05-15 11:35:43 -0400

    openshift example

---
+++ src/main/java/com/openshift/demo/openshiftdemo/OpenshiftDemoApplication.java
@@ -0,0 +1,13 @@
+package com.openshift.demo.openshiftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftDemoApplication.class, args);
+	}
+
+}
